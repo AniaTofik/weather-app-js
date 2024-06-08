4fb571da5d7e69c17dc4c03a07fbcd18acfe85b1
AniaTofik <anna.kiedel@onet.pl>
2024-06-08 21:34:37 +0200

    created conditional statement for weather images

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const API_KEY = "&appid=02e3b8e16ca9ab74967cb49577026069";
 const API_UNITS = "&units=metric";
 
 const getWeather = () => {
-  const city = input.value || "Toronto";
+  const city = input.value || "Gdańsk";
   const URL = API_LINK + city + API_KEY + API_UNITS;
 
   axios.get(URL).then((res) => {
@@ -26,6 +26,25 @@ const getWeather = () => {
     temperature.textContent = temp.toFixed(0) + "°C";
     humidity.textContent = hum + "%";
     weather.textContent = status.main;
+
+    if (status.id <= 232) {
+      photo.setAttribute("src", "../img/thunderstorm.png");
+    } else if (status.id > 232 && status.id <= 321) {
+      photo.setAttribute("src", "../img/drizzle.png");
+    } else if (status.id >= 500 && status.id <= 531) {
+      photo.setAttribute("src", "../img/rain.png");
+    } else if (status.id >= 600 && status.id <= 622) {
+      photo.setAttribute("src", "../img/ice.png");
+    } else if (status.id >= 701 && status.id <= 781) {
+      photo.setAttribute("src", "../img/fog.png");
+    } else if ((status.id = 800)) {
+      photo.setAttribute("src", "../img/sun.png");
+    } else if (status.id >= 801) {
+      photo.setAttribute("src", "../img/cloud.png");
+    } else {
+      photo.setAttribute("src", "../img/unknown.png");
+    }
   });
 };
+
 button.addEventListener("click", getWeather);
